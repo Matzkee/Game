3b7d18f5f8853a7047f32f455e0ce2703738d8c7
Mateusz Pietraszewski <pietraszewski.mateusz@gmail.com>
2014-04-20 19:22:15 +0100

    Movement & Particles
    Added simple movement script as well as engine particles

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+[System.Serializable]
+public class BoxEdges
+{
+	public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerControls : MonoBehaviour {
+
+	public float speed = 10.0f;
+	public BoxEdges edges;
+
+	void FixedUpdate () {
+		float xPos = Input.GetAxis("Horizontal");
+		float yPos = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3 (xPos, 0.0f, yPos);
+		rigidbody.velocity = movement * speed;
+
+		rigidbody.position = new Vector3 
+		(
+			Mathf.Clamp (rigidbody.position.x, edges.xMin, edges.xMax), // limit x axis
+			0.0f, 
+			Mathf.Clamp (rigidbody.position.z, edges.zMin, edges.zMax) // limit z axis
+		);
+	}
+}
